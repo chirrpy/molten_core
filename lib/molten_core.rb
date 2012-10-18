3d7d47863f667b19b3c13b5d5d5e0ebf50e5e231
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-18 15:17:49 -0500

    Remove STI code from molten_core and move it into stipend

--- lib/molten_core.rb
+++ lib/molten_core.rb
@@ -5,8 +5,6 @@ require 'molten_core/integer'
 require 'molten_core/string'
 require 'molten_core/action_controller/controller_resources'
 require 'molten_core/active_model/validations/inclusion_validator'
-require 'molten_core/single_table_inheritable'
-require 'molten_core/unprotected_inheritance'
 
 module MoltenCore
 end
